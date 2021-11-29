6b44d2bd9c43489fb1a025ad688bb24b233fc843
Natsu <62543597+Natsutoja@users.noreply.github.com>
2021-11-29 20:47:34 +0100

    Add files via upload

---
+++ java/Accessories.java
@@ -0,0 +1,9 @@
+public class Accessories {
+   public String material;
+   public String kolor;
+   public String cena;
+   public String wymiary;
+   public String waga;
+   public String nazwa;
+
+}
