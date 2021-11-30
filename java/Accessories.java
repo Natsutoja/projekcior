736925a8d75f658db2524adf6c3db9b827a54ded
Natsu <62543597+Natsutoja@users.noreply.github.com>
2021-11-30 14:07:16 +0100

    Update Accessories.java

--- java/Accessories.java
+++ java/Accessories.java
@@ -1,9 +1,20 @@
 public class Accessories {
    public String material;
+   public String nazwa;
    public String kolor;
    public String cena;
    public String wymiary;
-   public String waga;
-   public String nazwa;
+   public String  waga;
 
+   public Accessories(String material,String nazwa,String kolor,String cena,String wymiary,String waga){
+      this.material=material;
+      this.nazwa=nazwa;
+      this.kolor=kolor;
+      this.cena=cena;
+      this.wymiary=wymiary;
+      this.waga=waga;
+   }
+   public void Items(){
+      System.out.printf(material + " " + nazwa + " " + kolor + " " + cena + " " + wymiary + " " + waga +  "\n");
+   }
 }
