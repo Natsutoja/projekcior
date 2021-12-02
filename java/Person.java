27c82de91b3e82db21a87cbba81b576e12338e6e
Natsu <62543597+Natsutoja@users.noreply.github.com>
2021-12-02 16:22:50 +0100

    Update Person.java

--- java/Person.java
+++ java/Person.java
@@ -12,44 +12,4 @@ public class Person {
         this.adress = adress;
         this.email = email;
     }
-
-    public String getImie() {
-        return imie;
-    }
-
-    public void setImie(String imie) {
-        this.imie = imie;
-    }
-
-    public String getNazwisko() {
-        return nazwisko;
-    }
-
-    public void setNazwisko(String nazwisko) {
-        this.nazwisko = nazwisko;
-    }
-
-    public String getNrtel() {
-        return nrtel;
-    }
-
-    public void setNrtel(String nrtel) {
-        this.nrtel = nrtel;
-    }
-
-    public String getAdress() {
-        return adress;
-    }
-
-    public void setAdress(String adress){
-        this.adress = adress;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String adress){
-        this.email = email;
-    }
 }
